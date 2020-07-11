95c2b30eaa2080a2fbd1326acd2cbfd8e0aeb3ab
Ryan Richard <mrrarichard@gmail.com>
2020-07-11 06:24:25 +0200

    Scaffold the Login and Registraton Pages to allow customisation

--- HumanRazor/Startup.cs
+++ HumanRazor/Startup.cs
@@ -28,11 +28,6 @@ namespace HumanRazor
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(
-                    Configuration.GetConnectionString("DefaultConnection")));
-            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-                .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddRazorPages()
                 .AddRazorPagesOptions(options =>
                  {
