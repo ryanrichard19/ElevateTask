7d90fc36e74155b525e365d62abbf9472dad1b13
Ryan Richard <mrrarichard@gmail.com>
2020-07-11 09:36:58 +0200

    Added in a DTO library to be shared by Front End and Back End Projects

--- BackEnd/Models/User.cs
+++ BackEnd/Models/User.cs
@@ -1,16 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace BackEnd.Models
+﻿namespace BackEnd.Models
 {
-    public class User
+    public class User : HumanDTO.User
     {
-        public int Id { get; set; }
-        [Required]
-        [StringLength(200)]
-        public string EmailAddress { get; set; }
+
     }
 }
---
+++ HumanDTO/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace HumanDTO
+{
+    public class User
+    {
+        public int Id { get; set; }
+        [Required]
+        [StringLength(200)]
+        public string EmailAddress { get; set; }
+    }
+}
